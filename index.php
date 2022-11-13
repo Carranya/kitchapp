2e3be96d8812f2aad4406eb45a4b07ed46c7b979
Carranya <k.gong82@gmail.com>
2022-11-13 18:37:07 +0100

    changed title

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kitch-App</title>
+    <title>Kitch-Wiz</title>
     <link rel="stylesheet" href="css/output.css">
     <script src="./node_modules/tw-elements/dist/js/index.min.js"></script>
 </head>

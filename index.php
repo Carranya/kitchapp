d980769fae45ceb93d08049c6c7e36ea69e120e0
Carranya <k.gong82@gmail.com>
2022-11-13 10:30:02 +0100

    create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kitch-App</title>
+    <link rel="stylesheet" href="css/output.css">
+</head>
+<body>
+    <div class='bg-violet-100'>Test Tailwind CSS </div>
+    <?php
+        include "pages/test.php";
+    ?>
+</body>
+</html>

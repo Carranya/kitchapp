861b501a9007f67fb6edbf4f3161acd02ea6f67f
Carranya <k.gong82@gmail.com>
2022-11-13 11:19:08 +0100

    testing

--- index.php
+++ index.php
@@ -6,11 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kitch-App</title>
     <link rel="stylesheet" href="css/output.css">
+    <script src="./TW-ELEMENTS-PATH/dist/js/index.min.js"></script>
 </head>
 <body>
     <div class='bg-violet-100'>Test Tailwind CSS </div>
     <?php
-        include "pages/test.php";
+    [$twig] = bootstrap();
+        // include "pages/test.php";
+        $test = "Erfolgreich";
+        $test2 = "Ok";
+        echo $twig->render("test/testtwig.twig", ["test" => $test, "test2" => $test2]);
     ?>
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{php,html,twig,js}"],
+  content: ['./**/*.{php,html,twig,js}', './node_modules/tw-elements/dist/js/**/*.js'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('tw-elements/dist/plugin')],
 }

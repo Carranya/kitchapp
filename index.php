fb2294057212975025284a764c59b9716a9041c1
Carranya <k.gong82@gmail.com>
2022-11-13 18:34:55 +0100

    testing

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="css/output.css">
     <script src="./node_modules/tw-elements/dist/js/index.min.js"></script>
 </head>
-<body class='font-sans bg-violet-800'>
+<body class='font-sans bg-violet-300'>
     <div>
         <?php
             include "pages/menu.php";

ee4be0317c91a0a807fc104805680275d5558ba4
Carranya <k.gong82@gmail.com>
2022-11-16 16:19:15 +0100

    create load.php

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-    require_once ("functions/bootingPage.php");
-    require_once ("functions/pageFunctions.php");
-    require_once ("models/Model.php");
-    require_once ("models/Product.php");
-
-    include "pages/devPages/devButtons.php";
+    require_once ("./load.php");
 ?>
 
 <!DOCTYPE html>

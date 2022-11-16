65de0fe02e0a1e6c929ea8170a472d9edaeee0b3
Carranya <k.gong82@gmail.com>
2022-11-16 16:16:46 +0100

    added button to mysql

--- pages/devPages/devButtons.php
+++ pages/devPages/devButtons.php
@@ -1,3 +1,4 @@
 <div class='absolute top-0 right-0 bg-red-500'>
-    <a href='create.php' target='_blank'>Update tables</a><br>
+    <a href='phpmyadmin' target='_blank'>MySQL tables</a><br>
+    <a href='create.php' target='_blank'>Update tables</a>
 </div>

b30a637ac0d2a016b1e733d3eeca27a3be57caec
Carranya <k.gong82@gmail.com>
2022-11-16 16:16:14 +0100

    added model

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-    require_once "functions/bootingPage.php";
-    require_once "functions/pageFunctions.php";
+    require_once ("functions/bootingPage.php");
+    require_once ("functions/pageFunctions.php");
+    require_once ("models/Model.php");
+    require_once ("models/Product.php");
 
     include "pages/devPages/devButtons.php";
 ?>

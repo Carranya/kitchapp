225d7d8552ac6980d6fb6b59cba9fd0032233caf
Carranya <k.gong82@gmail.com>
2022-11-17 17:39:34 +0100

    mod shoppingList

--- pages/shopping.php
+++ pages/shopping.php
@@ -1,23 +1,4 @@
 <?php
-
-use \Kw\Models\Model;
-use \Kw\Models\Product;
-
-    $product = new Product;
-    $product->productName = "Butter";
-    $product->unit = "g";
-    $product->save();
-
-    $product2 = new Product;
-    $product2->productName = "Wasser";
-    $product2->unit = "L";
-    $product2->save();
-
-    $product3 = new Product;
-    $product3->productName = "Eier";
-    $product3->unit = "Stück";
-    $product3->save();
-
     include 'lists/shoppingList.php';
     include 'lists/inventoryList.php';
 ?>

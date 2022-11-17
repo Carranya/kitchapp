df1486d0789544adf9b8d6be1abdde1e2baa2086
Carranya <k.gong82@gmail.com>
2022-11-17 17:38:49 +0100

    add findData

--- load.php
+++ load.php
@@ -1,6 +1,7 @@
 <?php
     require_once ("./functions/bootingPage.php");
     require_once ("./functions/pageFunctions.php");
+    require_once ("./functions/findData.php");
     require_once ("./models/Model.php");
     require_once ("./models/Product.php");
 

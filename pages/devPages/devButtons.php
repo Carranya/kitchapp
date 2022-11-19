b0b6e3152930ac8a9badafd4ca41e1ee981bcfb7
Carranya <k.gong82@gmail.com>
2022-11-20 00:01:47 +0100

    cleaned up

--- pages/devPages/devButtons.php
+++ pages/devPages/devButtons.php
@@ -1,5 +1,14 @@
 <div class='absolute top-0 right-0 bg-red-500'>
-    <a href='phpmyadmin' target='_blank'>MySQL tables</a><br>
+    <a href='/phpmyadmin' target='_blank'>MySQL tables</a><br>
     <a href='create.php' target='_blank'>Update tables</a><br>
     <a href='test.php' target='_blank'>Test page</a>
 </div>
+
+<?php
+    function showVar($value){
+        echo "<pre>";
+            var_dump($value);
+        echo "</pre>";
+        die();
+    }
+?>

82f045179f3d5233f1deef190c6f243ff8b00c66
Carranya <k.gong82@gmail.com>
2022-11-20 00:01:30 +0100

    new test page

--- test.php
+++ test.php
@@ -6,10 +6,56 @@ use Kw\Models\Product;
 
 require_once("./load.php");
 
-$data = ['productName' => 'Butter', 'unit' => 'g'];
 
-modifyData('Product', 1, $data);
+// showVar($_SERVER);
+
+$data = ['productName' => 'Cola', 'unit' => 'Liter'];
+modifyData('Product', 2, $data);
+/* class Test {
+
+    public $container;
+
+    function show(){
+
+        foreach($this->container as $content){
+            foreach($content as $key => $value){
+                echo $key . " => " . $value . "<br>";
+            }
+        }
+    }
+}
+
+
+    function showAll(){
+    global $con;
+
+    $res = $con->query("SELECT * FROM products");
+    $container = [];
+    while($data = $res->fetch_assoc()){
+        $container[] = $data;
+    }
+
+    
+
+    return $container;
+    }
+
+$cont = showAll();
+
+$test = new Test;
+$test->container = $cont;
+$test->show();
+ */
+
+
+
+// $test = findAll(Product::class);
+
+/* foreach($container as $tes){
+    echo $tes->productName . "<br>";
+} */
+
+// print_r($test[0]->productName);
 
 
 ?>
-

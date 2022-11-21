206fe8cb20f285e196645542a86268072f170a4f
Carranya <k.gong82@gmail.com>
2022-11-21 08:34:52 +0100

    new mysql connection

--- functions/bootingPage.php
+++ functions/bootingPage.php
@@ -1,14 +1,21 @@
 <?php
 
     require_once ('./vendor/autoload.php');
-    require_once ('./settings/settings.php');
+    require_once ('./config/config.php');
 
     $loader = new \Twig\Loader\FilesystemLoader('templates');
     $twig = new \Twig\Environment($loader, [
         'cache' => FALSE,
     ]);
 
-    global $DB;
-    $con = new mysqli($DB['hostname'], $DB['username'], $DB['password'], $DB['database']);
+    function mysqlConnect(){
+        global $DB;
+        $con = "mysql:host=" . $DB['hostname'] . ";port=" . $DB['port'] . ";dbname=" . $DB['database'];
+        return new PDO($con, $DB['username'], $DB['password']);
+    }
     
+    function createSamples(){
+        global $DB;
+        return new mysqli($DB['hostname'], $DB['username'], $DB['password'], $DB['database']);
+    }
 ?>

88376d4a54484727ec695bd54d22f88af6d521e7
Carranya <k.gong82@gmail.com>
2022-11-21 08:35:59 +0100

    new productlist

---
+++ pages/lists/productsList.php
@@ -0,0 +1,13 @@
+<?php
+    use \Kw\Models\Model;
+    use \Kw\Models\Product;
+
+    global $twig;
+    $products = findData(Product::class);
+
+    echo $twig->render('products.twig',
+    ['products' => $products,
+    'currentPage' => $currentPage,
+    'pickToModify' => @$_POST['pickToModify']
+    ]);
+?>

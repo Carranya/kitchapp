6ed688c31011121517d7cf9b551fcf0e2d554dc0
Carranya <k.gong82@gmail.com>
2022-11-21 08:35:18 +0100

    new models

--- models/Product.php
+++ models/Product.php
@@ -6,4 +6,12 @@ use Kw\Models\Model;
 class Product extends Model {
     protected $table = "products";
     protected $orderBy = "id";
+    public $data = [];
+
+    public function inputData($productName, $unit){
+        $this->data = [
+            'productName' => $productName,
+            'unit' => $unit
+        ];
+    }
 }

9f5c9d59fdeb60f6d56bada4626a0d151dc635c8
Carranya <k.gong82@gmail.com>
2022-11-22 07:06:07 +0100

    added recipe page

--- load.php
+++ load.php
@@ -5,6 +5,7 @@
     require_once ("./models/Model.php");
     require_once ("./models/Inventory.php");
     require_once ("./models/Product.php");
+    require_once ("./models/Recipe.php");
 
     require_once("pages/devPages/devButtons.php");
 ?>

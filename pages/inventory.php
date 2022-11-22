6bec72315c85d89f29f7262305665338bb03bf9f
Carranya <k.gong82@gmail.com>
2022-11-22 07:06:59 +0100

    added inventory page

--- pages/inventory.php
+++ pages/inventory.php
@@ -1,8 +1,38 @@
 <form action='index.php?page=inventory' method='post'>
 
 <?php
+
+use Kw\Models\Model;
+use Kw\Models\Inventory;
+use Kw\Models\Product;
+
+    if(isset($_POST['modify'])){
+        $id = $_POST['modify'];
+        $saveData = new Inventory;
+        $saveData->inputData($_POST['productId'], $_POST['amount']);
+        $saveData->save($id);
+    }
+
+    if(isset($_POST['create'])){
+        $saveData = new Inventory;
+        $saveData->inputData($_POST['newProductId'], $_POST['newAmount']);
+        $saveData->save();
+    }
+
+    if(isset($_POST['delete'])){
+        $id = $_POST['delete'];
+        $saveData = new Inventory;
+        $saveData->delete($id);
+    }
+
+    if(isset($_POST['createProduct'])){
+        $saveData = new Product;
+        $saveData->inputData($_POST['newProductName'], $_POST['newUnit']);
+        $saveData->save();
+    }
+
     include 'lists/inventoryList.php';
-    include 'lists/productsList.php';
+    echo $twig->render('createProducts.twig');
 ?>
 
 </form>

4af5bb9b3f4a889079f5e06c4a637d74afc81312
Carranya <k.gong82@gmail.com>
2022-11-22 07:06:56 +0100

    added inventory page

--- models/Inventory.php
+++ models/Inventory.php
@@ -6,4 +6,11 @@ use Kw\Models\Model;
 class Inventory extends Model {
     protected $table = "inventory";
     protected $orderBy = "id";
+
+    public function inputData($productId, $amount){
+        $this->data = [
+            'productId' => $productId,
+            'amount' => $amount
+        ];
+    }
 }

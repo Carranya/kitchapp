19a9ef9808b136ece1fdff5003a4bfbc54806b18
Carranya <k.gong82@gmail.com>
2022-11-23 07:33:22 +0100

    added recipe page

--- pages/recipes.php
+++ pages/recipes.php
@@ -1,3 +1,5 @@
+<form action='index.php?page=recipes' method='post'>
+
 <?php
     use Kw\Models\Model;
     use Kw\Models\Recipe;
@@ -5,3 +7,9 @@
     $currentPage = 'recipes';
 
     include "lists/recipesList.php";
+
+    if(isset($_POST['showIngredients'])){
+        include "lists/ingredientsList.php";
+    }
+?>
+</form>

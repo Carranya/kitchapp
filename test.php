dde43e6a8069459425949aa17055f44c8cd8fae3
Carranya <k.gong82@gmail.com>
2022-11-23 11:45:36 +0100

    testing

--- test.php
+++ test.php
@@ -12,7 +12,79 @@ function xx($v){
 
 require_once("./load.php");
 
-$recipe = findData(Inventory::class);
 
-xx($recipe);
+/* class Listing{
+    public $list;
+    
+    public function __construct(Inventory $list){
+        $this->list = $list;
+    }
+    
+} */
+
+
+class Listing extends Model {
+    protected $table = "inventory";
+    protected $orderBy = "id";
+    
+    public function inputData($productId, $amount){
+        $this->data = [
+            'productId' => $productId,
+            'amount' => $amount
+        ];
+    }
+    
+    public function check(){
+        $ref = new \ReflectionObject($this);
+        $coll = [];
+        
+        foreach($ref->getProperties(\ReflectionProperty::IS_PUBLIC) as $item){
+            $coll[$item->getName()] = $this->{$item->getName()};
+        }
+        
+        return $coll;
+    }
+}
+
+function convert($items){
+    $productId = [];
+    foreach($items as $item){
+        foreach($item as $key => $value){
+            if($key == 'productId'){
+                $productId[] = $key;
+            }
+        }
+    }
+    return $productId;
+}
+
+function convert2($items){
+    // $productId['amount'] = 0;
+    foreach($items as $item){
+        foreach($item as $key => $value){
+            
+            $o = new Listing;
+            $o->$key = $value;
+            $o->check();
+            xx($o);
+        }
+    }
+    // return $productId;
+}
+
+$list = findData(Inventory::class);
+$prod = convert2($list);
+xx($prod);
+
+
+
+/* $test = new Listing;
+$test->list = $list;
+$test->check();
+
+    
+xx($test); */
+
+
+
 ?>

038f5f3142d2411a69a1dc2c316306653d5715d2
Carranya <k.gong82@gmail.com>
2022-11-23 10:57:56 +0100

    testing

--- test.php
+++ test.php
@@ -3,7 +3,16 @@
 
 use Kw\Models\Model;
 use Kw\Models\Product;
+use Kw\Models\Inventory;
+use Kw\Models\Recipe;
+
+function xx($v){
+    var_dump($v);
+}
 
 require_once("./load.php");
 
+$recipe = findData(Inventory::class);
+
+xx($recipe);
 ?>

d2d9aed42e60de2c5dc576e328509b3e2cae3fde
Carranya <k.gong82@gmail.com>
2022-11-24 07:47:22 +0100

    mod Model

--- models/Inventory.php
+++ models/Inventory.php
@@ -7,10 +7,42 @@ class Inventory extends Model {
     protected $table = "inventory";
     protected $orderBy = "id";
 
-    public function inputData($productId, $amount){
+    /* public function inputData($productId, $amount, $currentList, $id=0){
+        $check = [];
+        $index = 0;
+
+        foreach($currentList as $list){
+            foreach($list as $key => $value){
+                if($key ==  'productId'){
+                    $check[$key] = $value;
+                    if($check[$key] == $productId){
+                        $amount = $amount + $currentList[$index]['amount'];
+                        $id = $currentList[$index]['id'];
+                    }
+                }
+            }
+            $index++;    
+        }
+
         $this->data = [
             'productId' => $productId,
             'amount' => $amount
+        ];
+        return $id;
+    } */
+
+    public function inputData($productId, $amount){
+        
+        $this->productId = $productId;
+        $this->amount = $amount;
+        $this->createData();
+        
+    }
+
+    public function createData(){
+        $this->data = [
+            'productId' => $this->productId,
+            'amount' => $this->amount
         ];
     }
 }

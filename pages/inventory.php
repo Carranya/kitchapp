e0b8b992d36ac50f8078078217ee6358a8460944
Carranya <k.gong82@gmail.com>
2022-11-26 07:53:21 +0100

    cleanup inventory

--- pages/inventory.php
+++ pages/inventory.php
@@ -7,36 +7,36 @@ use Kw\Models\Inventory;
 use Kw\Models\Product;
 
 $currentPage = 'inventory';
-$currentList = findData(Inventory::class);
 
-    if(isset($_POST['modify'])){
-        $id = $_POST['modify'];
-        $saveData = new Inventory;
-        $saveData->inputData($_POST['productId'], $_POST['amount']);
-        $saveData->save($id);
-    }
+if(isset($_POST['modify'])){
+    $id = $_POST['modify'];
+    $saveData = new Inventory;
+    $saveData->inputData($_POST['productId'], $_POST['amount']);
+    $saveData->save($id);
+}
 
-    if(isset($_POST['create'])){
-        $saveData = new Inventory;
-        $saveData->inputData($_POST['newProductId'], $_POST['newAmount']);
-        $id = $saveData->check($currentList);
-        $saveData->save($id);
-    }
+if(isset($_POST['create'])){
+    $saveData = new Inventory;
+    $saveData->inputData($_POST['newProductId'], $_POST['newAmount']);
+    $currentList = findData(Inventory::class);
+    $id = $saveData->check($currentList);
+    $saveData->save($id);
+}
 
-    if(isset($_POST['delete'])){
-        $id = $_POST['delete'];
-        $deleteData = new Inventory;
-        $deleteData->delete($id);
-    }
+if(isset($_POST['delete'])){
+    $id = $_POST['delete'];
+    $deleteData = new Inventory;
+    $deleteData->delete($id);
+}
 
-    if(isset($_POST['createProduct'])){
-        $saveData = new Product;
-        $saveData->inputData($_POST['newProductName'], $_POST['newUnit']);
-        $saveData->save();
-    }
+if(isset($_POST['createProduct'])){
+    $saveData = new Product;
+    $saveData->inputData($_POST['newProductName'], $_POST['newUnit']);
+    $saveData->save();
+}
 
-    include 'lists/inventoryList.php';
-    echo $twig->render('createProducts.twig');
+include 'lists/inventoryList.php';
+echo $twig->render('createProducts.twig');
 ?>
 
 </form>

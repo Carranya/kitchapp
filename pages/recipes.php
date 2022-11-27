f824188e650393705b9f30ea95667b364f31d253
Carranya <k.gong82@gmail.com>
2022-11-27 07:13:22 +0100

    mod. recipe

--- pages/recipes.php
+++ pages/recipes.php
@@ -1,6 +1,7 @@
 <form action='index.php?page=recipes' method='post'>
 
 <?php
+    session_start();
     use Kw\Models\Model;
     use Kw\Models\Recipe;
     use Kw\Models\Ingredient;
@@ -45,10 +46,44 @@
         $saveData->save($id);
     }
 
+    if(isset($_POST['deleteIngredient'])){
+        $deleteData = new Ingredient;
+        $deleteData->delete($_POST['deleteIngredient']);
+    }
+
     include "lists/recipesList.php";
+    
+    /* if(isset($_POST['showIngredients'])){
+        if (@$id != $_POST['showIngredients']){
+            $id = $_POST['showIngredients'];
+        } else {
+            $id = $_POST['currentId'];
+        }
+        include "lists/ingredientsList.php";
+    } */
 
     if(isset($_POST['showIngredients'])){
+        $id = $_POST['showIngredients'];
+    } else if (isset($_POST['currentId'])){
+        if ($id != @$_POST['showIngredients']){
+            $id = $_POST['showIngredients'];
+        } else {
+            $id = $_POST['currentId'];
+        }
+    } else if (isset($_POST['create'])){
+        $newId = findDataByCol(Recipe::class, 'recipeName', $_POST['newRecipeName']);
+        // var_dump($newId);
+        $id = $newId[0]['id'];
+    }
+
+    if(isset($id)){
         include "lists/ingredientsList.php";
     }
+    /* if(isset($_POST['showIngredients'])){
+        $id = $_POST['showIngredients'];
+    } else {
+        $id = 1;
+    }
+    include "lists/ingredientsList.php"; */
 ?>
 </form>

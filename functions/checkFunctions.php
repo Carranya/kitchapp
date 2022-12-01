e6674bf98915e0f6e9e9ad74fb49dce490bb07bf
Carranya <k.gong82@gmail.com>
2022-12-01 07:44:42 +0100

    add Check Functions

--- functions/checkFunctions.php
+++ functions/checkFunctions.php
@@ -1,13 +1,25 @@
 <?php
 
-function recipeNameCheck($currentItems, $newName){
+function nameCheck($currentItems, $newName, $indexName){
     global $twig;
     $nameOccupied = 0;
     foreach($currentItems as $currentItem){
-        if($currentItem['recipeName'] == $newName){
-            echo $twig->render('errorSigns/errorRecipeName.twig');
+        if($currentItem[$indexName] == $newName){
+            echo $twig->render('errorSigns/errorName.twig');
             $nameOccupied++;
         }
     }
     return $nameOccupied;
 }
+
+/* function productNameCheck($currentItems, $newName){
+    global $twig;
+    $nameOccupied = 0;
+    foreach($currentItems as $currentItem){
+        if($currentItem['productName'] == $newName){
+            echo $twig->render('errorSigns/errorProductName.twig');
+            $nameOccupied++;
+        }
+    }
+    return $nameOccupied;
+} */

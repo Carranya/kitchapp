708e3c2ba74491e42ca1b541251c332159155500
Carranya <k.gong82@gmail.com>
2022-12-05 09:50:51 +0100

    create Active functions

---
+++ pages/lists/activeList.php
@@ -0,0 +1,17 @@
+<?php
+    use Kw\Models\Model;
+    use Kw\Models\Active;
+    use Kw\Models\Recipe;
+    // use Kw\Models\Inventory;
+    // use Kw\Models\Product;
+
+    global $twig;
+
+    $active = findData(Active::class);
+    $recipes = findData(Recipe::class);
+
+    echo $twig->render('active.twig',[
+        'active' => $active,
+        'recipes' => $recipes,
+    ]);
+?>

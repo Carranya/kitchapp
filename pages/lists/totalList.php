35770bd8a6f71241f12ce14d783d3298d7777c19
Carranya <k.gong82@gmail.com>
2022-12-07 14:22:44 +0100

    add totalList

--- pages/lists/totalList.php
+++ pages/lists/totalList.php
@@ -1,13 +1,15 @@
 <?php
     use Kw\Models\Model;
-    use Kw\Models\Inventory;
+    use Kw\Models\TotalList;
     use Kw\Models\Product;
 
     global $twig;
 
     $totalList = findData(TotalList::class);
+    $products = findData(Product::class);
 
-    echo $twig->render('inventory.twig',[
+    echo $twig->render('totalList.twig',[
         'totalList' => $totalList,
+        'products' => $products,
     ]);
 ?>

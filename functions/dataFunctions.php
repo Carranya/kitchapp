6634f4c610c0079b4fb7c1e77ad3754dafc97ba2
Carranya <k.gong82@gmail.com>
2022-12-07 14:22:33 +0100

    added function calculate total

--- functions/dataFunctions.php
+++ functions/dataFunctions.php
@@ -1,6 +1,10 @@
 <?php
 
+use Kw\Models\Active;
+use Kw\Models\Recipe;
+use Kw\Models\Ingredient;
 use Kw\Models\Product;
+use Kw\Models\TotalList;
 
 function createProduct($newProductName, $newUnit){
     $currentItems = findData(Product::class);
@@ -9,5 +13,46 @@ function createProduct($newProductName, $newUnit){
         $saveData = new Product;
         $saveData->inputData($newProductName, $newUnit);
         $saveData->save();
+    }
+}
+
+function refreshTotalList(){
+    $query = mysqlConnect();
+        $dropTable = $query->prepare("DROP TABLE TotalList");
+        $dropTable->execute();
+
+        $sql = "CREATE TABLE IF NOT EXISTS totalList (
+            id INT(255) NOT NULL AUTO_INCREMENT,
+            productId INT(255) NOT NULL,
+            amount INT(255) NOT NULL,
+            PRIMARY KEY (id)
+        )ENGINE=InnoDB, DEFAULT CHARSET=UTF8";
+
+        $createTable = $query->prepare($sql);
+        $createTable->execute();
+}
+
+function calculateTotalList(){
+    refreshTotalList();
+    $actives = findData(Active::class);
+    $recipes = findData(Recipe::class);
+
+    foreach($actives as $active){
+        foreach($recipes as $recipe){
+            if($active['recipeId'] == $recipe['id']){
+                $ingredients = findDataByCol(Ingredient::class, 'recipeId', $recipe['id']);
+                foreach($ingredients as $ingredient){
+                    $saveData = new TotalList;
+                    $currentItems = findData(TotalList::class);
+                    $amount = $ingredient['amount'] * $active['amount'];
+                    $saveData->inputData(
+                        $ingredient['productId'],
+                        $amount,
+                    );
+                    $id = $saveData->check($currentItems);
+                    $saveData->save($id);
+                }
+            }
+        }
     }
 }

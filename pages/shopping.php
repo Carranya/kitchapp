96aa46428e56e7ed738895d758c4f1f9c7bc81a9
Carranya <k.gong82@gmail.com>
2022-12-22 11:05:33 +0100

    add shopping functions

--- pages/shopping.php
+++ pages/shopping.php
@@ -2,13 +2,49 @@
 
 <?php
 
+use Kw\Models\Model;
+use Kw\Models\Shopping;
+use Kw\Models\Inventory;
+use Kw\Models\Product;
+
 $currentPage = 'shopping';
 
-    include 'lists/shoppingList.php';
+if(isset($_POST['modify'])){
+    $id = $_POST['modify'];
+    $saveData = new Shopping;
+    $saveData->inputData($_POST['productId'], $_POST['amount']);
+    $saveData->save($id);
+}
 
-    if(isset($_POST['showInventory'])){
-        include 'lists/inventoryList.php';
-    }
+if(isset($_POST['create'])){
+    $saveData = new Shopping;
+    $saveData->inputData($_POST['newProductId'], $_POST['newAmount']);
+    $currentList = findData(Shopping::class);
+    $id = $saveData->check($currentList);
+    $saveData->save($id);
+}
+
+if(isset($_POST['delete'])){
+    $id = $_POST['delete'];
+    $deleteData = new Shopping;
+    $deleteData->delete($id);
+}
+
+if(isset($_POST['createProduct'])){
+    $saveData = new Product;
+    $saveData->inputData($_POST['newProductName'], $_POST['newUnit']);
+    $saveData->save();
+}
+
+include 'lists/shoppingList.php';
+
+if(isset($_POST['showInventory'])){
+    include 'lists/inventoryList.php';
+}
+
+if(isset($_POST['addProducts'])){
+    echo $twig->render('createProducts.twig');
+}
 ?>
 
 </form>
